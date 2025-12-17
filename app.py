efeabc1f8d4337da603e0791cb737d93347dc9a6
adrianaramirez-hash <adriana.ramirez@udlondres.com>
2025-12-17 08:48:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import streamlit as st
 # Configuración básica de la página
 st.set_page_config(page_title="Dirección Académica", layout="wide")
 
-# URL de la imagen del escudo (versión directa de tu enlace de Drive)
-logo_url = "https://drive.google.com/uc?export=view&id=1qZIKvyxFmhnFrgMEYUIMp92KOn5zca2G"
+# Escudo de la UDL desde el repositorio
+logo_url = "udl_logo.png"
+
 
 # Encabezado con escudo + texto
 col1, col2 = st.columns([1, 4])
